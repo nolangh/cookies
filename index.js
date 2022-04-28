453f5b325667259faa6a31726abb0961a8bd624a
nolangh <nolanghiggins@gmail.com>
2022-04-28 09:24:43 -0600

    -a

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
-
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-	res.cookie("myCooie", "myValue");
+	res.cookie("myCooie", "myValue", { secure: true });
+	res.cookie("cookie#2", "cookieValue#2", { maxAge: 10000 });
 	res.end("hello world");
+	console.log(res.getHeaders());
+	console.log(req.cookie);
 });
 
-app.get();
+app.listen(`${port}`, () => {
+	console.log(`Server running on locahost: ${port}`);
+});
