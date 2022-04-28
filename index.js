822573369e064483f4510f9263df3062ac34ff96
nolangh <nolanghiggins@gmail.com>
2022-04-28 10:09:31 -0600

    -a

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const port = process.env.PORT || 8080;
 const app = express();
-
 app.use(cookieParser());
+
+app.use(
+	session({
+		secret: "test string that remains secret",
+		resave: false,
+		saveUninitialized: false,
+		cookie: { maxAge: 100000 },
+	})
+);
+
+//ANCHOR this is middleware that calls next
+app.use((req, res, next) => {
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.cookie("myCooie", "myValue", { secure: true });
@@ -11,6 +25,20 @@ app.get("/", (req, res) => {
 	res.end("hello world");
 	console.log(res.getHeaders());
 	console.log(req.cookie);
+});
+
+app.get("/views", (req, res) => {
+	if (!req.session.views) {
+		req.session.views = 1;
+		res.end("Web Page\n");
+		res.write(`views: ${req.session.views}`);
+	} else {
+		req.session.views++;
+		res.write("Web Page\n");
+		res.write(`views: ${req.session.views}\n`);
+		res.write(`expires in: ${req.session.cookie.maxAge / 1000} seconds`);
+		res.end();
+	}
 });
 
 app.listen(`${port}`, () => {
